bd0d322cbc06771dd62f4d60642b84c6bb61d2f6
Kaitlyn Chan <sc22kc2@leeds.ac.uk>
2024-02-26 14:49:58 +0000

    Page layout with buttons

---
+++ src/routes/payments/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import Weekly from './weekly.svelte'
+    import Monthly from './monthly.svelte'
+    import Yearly from './yearly.svelte'
+
+</script>
+
+<body>
+    <br>
+    <h1> <center> Payment Plans </center></h1>
+    <br>
+    <div class="flex">
+        <Weekly />
+        <Monthly />
+        <Yearly />
+    </div>
+</body>
+
+
+<style>
+    h1{
+        font-size: 225%;
+    }
+    body{
+        font-family: "Poppins";
+    }
+    .flex{
+		display: flex;
+		justify-content: space-evenly;
+        height:100%;
+	}
+</style>
