032f6b5dc9365dd5be69cb0a89291ccdd34bfe29
ayomide soruna <sc22as2@leeds.ac.uk>
2024-02-26 16:10:17 +0000

    Friends page functionality incomplete

---
+++ src/routes/friends/friendlist.svelte
@@ -0,0 +1,58 @@
+<script>
+  export let friends = [];
+  export let filterText = '';
+
+  function filterFriends() {
+    return friends.filter(friend =>
+      friend.name.toLowerCase().includes(filterText.toLowerCase())
+    );
+  }
+
+  function deleteFromFriends(friend) {
+    // Function for deleting friends not implemented yet
+    console.log('Delete', friend.name, 'to friends list');
+  }
+</script>
+
+<style>
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+
+  .profile-pic {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    background-color: #ccc;
+    margin-right: 10px;
+  }
+
+  .delete-button {
+    margin-left: 10px;
+    padding: 5px 10px;
+    background-color: #000a0641;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+</style>
+
+<div>
+  <ul>
+    {#each filterFriends() as friend}
+      <li>
+        <div class="profile-pic"></div>
+        <span>{friend.name}</span>
+        <button on:click={() => deleteFromFriends(friend)} class="delete-button">Delete</button>
+      </li>
+    {/each}
+  </ul>
+</div>
