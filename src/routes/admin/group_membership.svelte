d42a7b03e65c572d411ccaaf71001a4fff8ac3f5
HariC02 <125207801+HariC02@users.noreply.github.com>
2024-03-10 22:59:21 +0000

    Admin Panel

---
+++ src/routes/admin/group_membership.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+    import type { group_membership } from '$lib/interfaces';
+
+    export let group_memberships: group_membership[];
+
+    const handleUpdate = async (group_membership: group_membership) => {
+        const formData = new FormData();
+        formData.append('type', 'updateGroupMembership');
+        formData.append('id', group_membership.id.toString());
+        if (group_membership.group_id) formData.append('group_id', group_membership.group_id.toString());
+        if (group_membership.user_id) formData.append('user_id', group_membership.user_id.toString());
+        if (group_membership.admin) formData.append('admin', group_membership.admin.toString());
+
+        try {
+            if (!formData.has('id')) {
+                console.error('No ID provided');
+                return;
+            }
+            if (!formData.has('type')) {
+                console.error('No type provided');
+                return;
+            }
+            const response = await fetch('/admin', {
+                method: 'POST',
+                body: formData,
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
+                console.log(result.message || 'Update successful');
+            } else {
+                console.error(result.message || 'Update failed');
+            }
+        } catch (error) {
+            console.error('Error during update:', error);
+        }
+    };
+
+    const deleteGroupMembership = async (group_membership: group_membership) => {
+        const formData = new FormData();
+        formData.append('type', 'deleteGroupMembership');
+        formData.append('id', group_membership.id.toString());
+
+        try {
+            if (!formData.has('id')) {
+                console.error('No ID provided');
+                return;
+            }
+            if (!formData.has('type')) {
+                console.error('No type provided');
+                return;
+            }
+            const response = await fetch('/admin', {
+                method: 'POST',
+                body: formData,
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
+                console.log(result.message || 'Delete successful');
+            } else {
+                console.error(result.message || 'Delete failed');
+            }
+        } catch (error) {
+            console.error('Error during delete:', error);
+        }
+    };
+    var lockedFields = 1;
+</script>
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Group ID</th>
+            <th>User ID</th>
+            <th>Admin</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each group_memberships as group_membership}
+            {#if lockedFields == 1}
+                <tr>
+                    <td>{group_membership.id}</td>
+                    <td>{group_membership.group_id}</td>
+                    <td>{group_membership.user_id}</td>
+                    <td>{group_membership.admin}</td>
+                </tr>
+            {/if}
+            {#if lockedFields == 0}
+                <tr>
+                    <td>{group_membership.id}</td>
+                    <td><input type="text" bind:value={group_membership.group_id} /></td>
+                    <td><input type="text" bind:value={group_membership.user_id} /></td>
+                    <td><input type="text" bind:value={group_membership.admin} /></td>
+                    <button on:click={() => handleUpdate(group_membership)}>Submit</button>
+                    <button on:click={() => deleteGroupMembership(group_membership)}>Delete</button>
+                </tr>
+            {/if}
+        {/each}
+        <button on:click={() => (lockedFields = lockedFields ? 0 : 1)}>Toggle Edit</button>
+    </tbody>
+</table>
---
+++ src/routes/admin/route_coordinates.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+    import type { route_coordinate } from '$lib/interfaces';
+    import Decimal from 'decimal.js';
+    export let route_coordinates: route_coordinate[];
+
+    const handleUpdate = async (route_coordinate: route_coordinate) => {
+        const formData = new FormData();
+        formData.append('type', 'updateRouteCoordinate');
+        formData.append('id', route_coordinate.id.toString());
+        if (route_coordinate.route_id) formData.append('route_id', route_coordinate.route_id.toString());
+        if (route_coordinate.latitude) formData.append('latitude', route_coordinate.latitude.toString());
+        if (route_coordinate.longitude) formData.append('longitude', route_coordinate.longitude.toString());
+        if (route_coordinate.order_position)
+            formData.append('order_position', route_coordinate.order_position.toString());
+
+        try {
+            if (!formData.has('id')) {
+                console.error('No ID provided');
+                return;
+            }
+            if (!formData.has('type')) {
+                console.error('No type provided');
+                return;
+            }
+            const response = await fetch('/admin', {
+                method: 'POST',
+                body: formData,
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
+                console.log(result.message || 'Update successful');
+            } else {
+                console.error(result.message || 'Update failed');
+            }
+        } catch (error) {
+            console.error('Error during update:', error);
+        }
+    };
+
+    const deleteRouteCoordinate = async (route_coordinate: route_coordinate) => {
+        const formData = new FormData();
+        formData.append('type', 'deleteRouteCoordinate');
+        formData.append('id', route_coordinate.id.toString());
+
+        try {
+            if (!formData.has('id')) {
+                console.error('No ID provided');
+                return;
+            }
+            if (!formData.has('type')) {
+                console.error('No type provided');
+                return;
+            }
+            const response = await fetch('/admin', {
+                method: 'POST',
+                body: formData,
+            });
+
+            const result = await response.json();
+
+            if (response.ok) {
+                console.log(result.message || 'Delete successful');
+            } else {
+                console.error(result.message || 'Delete failed');
+            }
+        } catch (error) {
+            console.error('Error during delete:', error);
+        }
+    };
+
+    var lockedFields = 1;
+</script>
+
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Route ID</th>
+            <th>Latitude</th>
+            <th>Longitude</th>
+            <th>Order Position</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each route_coordinates as route_coordinate}
+            {#if lockedFields == 1}
+                <tr>
+                    <td>{route_coordinate.id}</td>
+                    <td>{route_coordinate.route_id}</td>
+                    <td>{route_coordinate.latitude}</td>
+                    <td>{route_coordinate.longitude}</td>
+                    <td>{route_coordinate.order_position}</td>
+                </tr>
+            {/if}
+            {#if lockedFields == 0}
+                <tr>
+                    <td>{route_coordinate.id}</td>
+                    <td><input type="text" bind:value={route_coordinate.route_id} /></td>
+                    <td><input type="text" bind:value={route_coordinate.latitude} /></td>
+                    <td><input type="text" bind:value={route_coordinate.longitude} /></td>
+                    <td><input type="text" bind:value={route_coordinate.order_position} /></td>
+                    <button on:click={() => handleUpdate(route_coordinate)}>Submit</button>
+                    <button on:click={() => deleteRouteCoordinate(route_coordinate)}>Delete</button>
+                </tr>
+            {/if}
+        {/each}
+        <button on:click={() => (lockedFields = lockedFields ? 0 : 1)}>Toggle Edit</button>
+    </tbody>
+</table>
