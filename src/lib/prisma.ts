22479ec2a6dc502c4b85fc35e4333473ded015d0
Abbie Hewitt <sc22ah3@leeds.ac.uk>
2024-03-10 15:00:46 +0000

    Merging to dev branch.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,4 @@
-// src/lib/prisma.ts
+import {PrismaClient} from '@prisma/client';
 
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-export default prisma
+const prisma = new PrismaClient();
+export default prisma;
