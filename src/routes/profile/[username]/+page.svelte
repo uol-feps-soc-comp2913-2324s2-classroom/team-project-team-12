21bc8a69c1c138b8881c0f4ebb80559828291521
DylanDD12 <dylandickinson@gmail.com>
2024-03-11 21:56:55 +0000

    Privacy
    Added code to protect private users information.

--- src/routes/profile/[username]/+page.svelte
+++ src/routes/profile/[username]/+page.svelte
@@ -9,20 +9,42 @@
 
   let activeTab = 'Friends';
   let tabs = ['Friends', 'Groups', 'Routes'];
+  let privacy = 0;
 
   function setActiveTab(tab) {
     activeTab = tab;
   }
 
+  // Generating dummy data for user friends
+  let userFriends = [
+      { 
+          profile_picture: "https://randomuser.me/api/portraits/men/3.jpg",
+          first_name: "Michael",
+          last_name: "Johnson",
+          username: "michio1998"
+      },
+      { 
+          profile_picture: "https://randomuser.me/api/portraits/women/4.jpg",
+          first_name: "Emily",
+          last_name: "Williams",
+          username: "em123"
+        },
+      { 
+          profile_picture: "https://randomuser.me/api/portraits/women/7.jpg",
+          first_name: "Anna",
+          last_name: "Smith",
+          username: "annanana"
+      }
+  ];
 </script>
 
 <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
  
 <style>
-
     body {
         overflow: hidden; /* Hide scrollbars */
     }
+    
     
     .container {
         height: 100vh;
@@ -89,6 +111,20 @@
         box-sizing: border-box;
     }
 
+    .tab-content-priv {
+        border: 1px solid #ccc;
+        border-radius: 0 0 5px 5px;
+        border-top: none;
+        width: 100%;
+        height: 100%;
+        box-sizing: border-box;
+        display: flex;  
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+    
+
     .name{
         font-size: 20px;
         font-family: "Poppins";
@@ -100,6 +136,86 @@
         color: #1c1c1c;
         margin-bottom: 10px;
     }
+
+    .friend {
+        display: flex;
+        align-items: center;
+        margin: 20px;
+        border: 1px solid #ccc;
+        padding: 20px;
+    }
+
+    .friend-profile-picture {
+        width: 50px;
+        height: 50px;
+        overflow: hidden;
+        border-radius: 50%;
+        margin-right: 15px;
+    }
+
+    .friend-profile-picture img {
+        width: 100%;
+        height: 100%;
+    }
+
+    .friend-details {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .friend-name {
+        font-size: 16px;
+        font-family: "Poppins";
+        color: #1c1c1c;
+    }
+
+    .user-name {
+        font-size: 13px;
+        font-family: "Poppins";
+        color: #1c1c1c;
+    }
+
+    .button-container {
+        margin-left: auto; /* Moves the button to the far right */
+    }
+
+    .add-button {
+        padding: 5px 10px;
+        background-color: #007bff;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        font-family: "Poppins";
+        font-size: 14px;
+    }
+
+    .add-button:hover {
+        background-color: #0056b3;
+    }
+
+    .private-message {
+        text-align: center;
+        font-size: 18px;
+        color: #1c1c1c;
+        margin-bottom: 15px;
+    }
+
+    .request-button {
+        padding: 10px 20px;
+        background-color: #007bff;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        font-family: "Poppins";
+        font-size: 14px;
+    }
+
+    .request-button:hover {
+        background-color: #0056b3;
+    }
+
 </style>
 
 <body>
@@ -108,20 +224,42 @@
     <div class="name">{user.first_name} {user.last_name}</div>
     <div class="username">@{user.username} <i class="fa-solid fa-plus"></i></div>
     <div class="profile-container">
-      <div class="tabs">
-        {#each tabs as tab}
-          <button class="tab" class:active={activeTab === tab} on:click={() => setActiveTab(tab)}>{tab}</button>
-        {/each}
-      </div>
-      <div class="tab-content">
-        {#if activeTab === 'Routes'}
-        <p>This is content for User Data.</p>
-        {:else if activeTab === 'Groups'}
-        <p>This is content for User Groups.</p>
-        {:else if activeTab === 'Friends'}
-        <p>This is content for User Friends.</p>
-        {/if}
-      </div>
+        <div class="tabs">
+            {#each tabs as tab}
+            <button class="tab" class:active={activeTab === tab} on:click={() => setActiveTab(tab)}>{tab}</button>
+            {/each}
+        </div>
+        {#if privacy == 1}
+        <div class="tab-content">
+            {#if activeTab === 'Routes'}
+                <p>This is content for User Data.</p>
+            {:else if activeTab === 'Groups'}
+                <p>This is content for User Groups.</p>
+            {:else if activeTab === 'Friends'}
+                <div class="friends-list">
+                    {#each userFriends as friend}
+                        <div class="friend">
+                            <div class="friend-profile-picture">
+                                <img src={friend.profile_picture} alt="" />
+                            </div>
+                            <div class="friend-details">
+                                <div class="friend-name">{friend.first_name} {friend.last_name}</div>
+                                <div class="user-name">@{friend.username}</div>
+                            </div>
+                            <div class="button-container">
+                                <button class="add-button">Add</button>
+                            </div>
+                        </div>
+                    {/each}
+                </div>
+            {/if}
+        </div>
+    {:else}
+        <div class="tab-content-priv">
+            <p class="private-message">This user's profile is private.</p>
+            <button class="request-button">Send Request</button>
+        </div>
+    {/if}
     </div>
   </div>
 </body>
