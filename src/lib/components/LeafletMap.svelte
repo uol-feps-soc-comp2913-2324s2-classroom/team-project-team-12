f75de823a8d0ebad521a544f12e891a2909d3a4a
cbouch-uni <125413783+cbouch-uni@users.noreply.github.com>
2024-03-22 18:45:17 +0000

    temporarily made navbar exclusive to /map

--- src/lib/components/LeafletMap.svelte
+++ src/lib/components/LeafletMap.svelte
@@ -86,4 +86,4 @@
     };
 </script>
 
-<div style="margin: 0; padding: 0; width: 100%; height: 100%;" use:mapHandler />
+<div style="margin: 0; padding: 0; width: 100vw; height: 100vh;" use:mapHandler />
