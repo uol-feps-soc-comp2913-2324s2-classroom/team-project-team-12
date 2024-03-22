450f7422990a00e0d8634ffeafa03dc48a6b7d20
cbouch-uni <125413783+cbouch-uni@users.noreply.github.com>
2024-03-22 13:51:39 +0000

    moved map to /map route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,3 @@
-<script lang="ts">
-    import { LeafletMap } from '$lib';
-
-    export let data;
-</script>
-
-<svelte:head>
-    <link
-        rel="stylesheet"
-        href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-        crossorigin=""
-    />
-</svelte:head>
-
-<div style="position: absolute; top: 0; left: 0;">
-    <LeafletMap {...data} />
-</div>
+<title>Journeys</title>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { LeafletMap } from '$lib';
+
+    export let data;
+</script>
+
+<svelte:head>
+    <link
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+        crossorigin=""
+    />
+</svelte:head>
+
+<div style="position: absolute; top: 0; left: 0;">
+    <LeafletMap {...data} />
+</div>
