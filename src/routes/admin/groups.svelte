aaa668bd126abdcc5e6e75a1a2bff39afc763b03
HariC02 <125207801+HariC02@users.noreply.github.com>
2024-03-28 18:37:16 +0000

    Corrected Groups Page

--- src/routes/admin/groups.svelte
+++ src/routes/admin/groups.svelte
@@ -134,9 +134,9 @@
 <div>
     Results Per Page
     <select bind:value={groupsPerPage} on:change={() => currentPage = 0}>
-        <option value={1}>10</option>
-        <option value={2}>25</option>
-        <option value={5}>50</option>
-        <option value={10}>100</option>
+        <option value={10}>10</option>
+        <option value={25}>25</option>
+        <option value={50}>50</option>
+        <option value={100}>100</option>
     </select>
 </div>
