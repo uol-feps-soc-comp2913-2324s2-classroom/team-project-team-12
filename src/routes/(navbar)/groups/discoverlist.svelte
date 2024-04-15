497781eef9bcbdc35e9d09392a3d1b36c55edfb5
AyoSoruna <sc22as2@leeds.ac.uk>
2024-04-15 23:56:01 +0100

    route to groups
    sends user to group page after clicking group in list

--- src/routes/(navbar)/groups/discoverlist.svelte
+++ src/routes/(navbar)/groups/discoverlist.svelte
@@ -77,7 +77,7 @@
     <input type="text" bind:value={searchTermGroup} placeholder="Search..." class="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:ring focus:border-blue-300 dark:bg-gray-800 dark:border-gray-600 dark:text-white" />
     {#each filteredGroups as group}
       <ListgroupItem class="flex items-center justify-between text-base font-semibold gap-2">
-          <a rel="external" href="../profile/{group.name}" class="flex items-center font-semibold text-gray-900 dark:text-white">
+          <a rel="external" href="../group/{group.name}" class="flex items-center font-semibold text-gray-900 dark:text-white">
             <span>{group.name}</span>
           </a>
           <button on:click={() => joinGroup(group)} class="flex items-center p-1 text-sm font-medium text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-red-500 hover:underline rounded-b-lg">
@@ -93,7 +93,7 @@
     <input type="text" bind:value={searchTermRequested} placeholder="Search..." class="w-full px-4 py-2 mb-4 border rounded-md focus:outline-none focus:ring focus:border-blue-300 dark:bg-gray-800 dark:border-gray-600 dark:text-white" />
     {#each filteredRequested as group}
       <ListgroupItem class="flex items-center justify-between text-base font-semibold gap-2">
-          <a rel="external" href="../profile/{group.groups.name}" class="flex items-center font-semibold text-gray-900 dark:text-white">
+          <a rel="external" href="../group/{group.groups.name}" class="flex items-center font-semibold text-gray-900 dark:text-white">
             <span>{group.groups.name}</span>
           </a>
           <button on:click={() => cancelGroupRequest(group)} class="flex items-center p-1 text-sm font-medium text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-red-500 hover:underline rounded-b-lg">
