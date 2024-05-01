902ce874edf926e852c9fc26821ac29aec641651
AyoSoruna <sc22as2@leeds.ac.uk>
2024-05-01 17:36:56 +0100

    routes tab for groups

--- src/routes/(navbar)/group/[name]/+page.svelte
+++ src/routes/(navbar)/group/[name]/+page.svelte
@@ -5,9 +5,16 @@
     export const members = data.members;
     export const creator = data.creator;
     export const memberCount = data.memberCount;
+    export const groupRouteEntries = data.groupRouteEntryObj;
+    export const currentUser = data.user;
+    import SingleRoute from "$lib/components/SingleRoute.svelte";
 
-    import { Button, Card, Tabs, TabItem, Listgroup, Avatar, Indicator } from 'flowbite-svelte';
+    import { Button, Card, Tabs, TabItem, Listgroup, ListgroupItem, Avatar, Heading, List, Li } from 'flowbite-svelte';
+    import User from "../../admin/user.svelte";
+    
 
+    console.log("suicide", groupRouteEntries.routes);
+    
     function getInitials(fullName) {
         // Split the full name into an array of words
         const nameArray = fullName.split(" ");
@@ -24,6 +31,43 @@
         return initials;
     }
 
+    const getRouteDuration = (route: RouteEntry) => {
+        let date = new Date(0);
+        date.setSeconds(route.completionTime);
+        return date.toISOString().substring(11, 19);
+    };
+
+    const deleteRouteFromGroup = (route: RouteEntry) => {
+    //console.log('adding ', route.name, 'to group ', route.group);
+    const formData = new FormData();
+    formData.append('type', 'deleteRouteFromGroup');
+    formData.append('routeID', route.id.toString());
+    formData.append('userID', currentUser.id.toString());
+    fetch('/routes', {
+        method: 'POST',
+        body: formData
+    }).then(response => response.json())
+        .then(data => {
+            let actualResponse = data.data;
+            actualResponse = JSON.parse(actualResponse);
+            //update the groupRouteEntries with the new group
+            if (actualResponse[1] == 200) {
+                alertStatus = 2;
+                let newGroupRouteEntries = groupRouteEntries.map(groupRouteEntry => {
+                if (groupRouteEntry.group_name == route.group) {
+                        groupRouteEntry.routes.push(route);
+                        alertStatus = 1;
+                }
+                return groupRouteEntry;
+            });
+            }
+            
+        })
+        .catch((error) => {
+           
+        });
+}
+
 </script>
  
 <style>
@@ -38,7 +82,19 @@
         justify-content: center;
         align-items: center;
         flex-direction: column;
+
+    
     }
+
+    .map-container {
+        width: 100px;
+        height: 100px;
+        overflow: hidden;
+        margin-right: 15px;
+    }
+
+
+    
 </style>
 
 <body>
@@ -72,6 +128,34 @@
                             </div>
                         </div>
                     </Listgroup>
+                </TabItem>
+                <TabItem class="w-full">
+                    <span slot="title">Routes</span>
+                    {#if groupRouteEntries.length == 0}
+                            <div class="flex items-center space-x-4 rtl:space-x-reverse">User has no Routes.</div>
+                    {:else}
+                        <Listgroup items={groupRouteEntries.routes} let:item class="border-0 dark:!bg-transparent">
+                            <div class="flex items-center space-x-4 rtl:space-x-reverse">
+                                <div class="map-container">
+                                    <SingleRoute route={item}/>
+                                </div>
+                                <div class="flex-1 min-w-0">
+                                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                        {item.name}
+                                    </p>
+                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        Created by: {item.creator}
+                                    </p>
+                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        Date Created: {item.createdOn.toLocaleString()}
+                                    </p>
+                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                        Route Duration: {getRouteDuration(item)}
+                                    </p>
+                                </div>
+                            </div>
+                        </Listgroup>
+                    {/if}
                 </TabItem>
             </Tabs>
         </Card>
